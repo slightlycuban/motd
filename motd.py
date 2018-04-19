51a8286104e4b770eb10d3eeef4de29f9c1c40f1
Michael Tracy <slightlycuban@gmail.com>
2018-04-19 11:23:28 -0400

    Add rendering of templates

--- motd.py
+++ motd.py
@@ -1,13 +1,20 @@
 import os
+import random
 
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
+messages = [
+    'What looks like a cat, flies like a bat, brays like a donkey, and plays like a monkey? A: Nothing.'
+]
+
 
 @app.route('/')
 def get_motd():
-    return "Hello, world!"
+    pick_one = random.randint(0, len(messages) - 1)
+    message = messages[pick_one]
+    return render_template('index.html', message=message)
 
 
 if __name__ == "__main__":
